ff5cad94bd06cf2c1156ae4e81c139fe52431ead
himza25 <himza.of.theking95@gmail.com>
2023-10-24 09:44:25 -0700

    Implement Bubble sort algorithm

--- sort.h
+++ sort.h
@@ -4,7 +4,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/* Data structure for doubly linked list */
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
 typedef struct listint_s
 {
     const int n;
