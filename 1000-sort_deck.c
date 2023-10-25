4315f82b97e6d93b9eef971433a291acfd3a2047
himza25 <himza.of.theking95@gmail.com>
2023-10-24 17:19:54 -0700

    Implement deck sort algorithm

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -1,62 +1,78 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "deck.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * compare_cards - Compares two cards.
- * @a: Pointer to the first card.
- * @b: Pointer to the second card.
- * Return: Difference between cards.
+ * card_value - Get card's numerical value.
+ * @value: Card name.
+ * Return: Numerical value of the card.
  */
-int compare_cards(const void *a, const void *b)
+int card_value(const char *value)
 {
-	const deck_node_t *nodeA = *(const deck_node_t **)a;
-	const deck_node_t *nodeB = *(const deck_node_t **)b;
-	const char *values[] = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9",
-			"10", "Jack", "Queen", "King"};
-	int i, valueA, valueB;
-
-	for (i = 0; i < 13; i++)
-	{
-		if (strcmp(nodeA->card->value, values[i]) == 0)
-			valueA = i;
-		if (strcmp(nodeB->card->value, values[i]) == 0)
-			valueB = i;
-	}
-
-	if (nodeA->card->kind == nodeB->card->kind)
-		return (valueA - valueB);
-
-	return ((int)nodeA->card->kind - (int)nodeB->card->kind);
+	if (strcmp(value, "Ace") == 0)
+		return (1);
+	if (strcmp(value, "Jack") == 0)
+		return (11);
+	if (strcmp(value, "Queen") == 0)
+		return (12);
+	if (strcmp(value, "King") == 0)
+		return (13);
+	return (atoi(value));
 }
 
 /**
- * sort_deck - Sorts a deck of cards.
- * @deck: Pointer to the head of the deck.
+ * swap_nodes - Swap two nodes in a list.
+ * @node1: First node.
+ * @node2: Second node.
+ */
+void swap_nodes(deck_node_t **node1, deck_node_t **node2)
+{
+	deck_node_t *tmp = (*node1)->prev;
+	(*node1)->prev = (*node2)->prev;
+	(*node1)->next = (*node2)->next;
+	(*node2)->prev = tmp;
+	(*node2)->next = (*node1);
+	if ((*node1)->prev)
+		(*node1)->prev->next = (*node1);
+	if ((*node1)->next)
+		(*node1)->next->prev = (*node1);
+	if ((*node2)->prev)
+		(*node2)->prev->next = (*node2);
+	(*node2)->next->prev = (*node2);
+}
+
+/**
+ * sort_deck - Sort a deck of cards.
+ * @deck: Pointer to the head of the list.
  */
 void sort_deck(deck_node_t **deck)
 {
-	deck_node_t **array, *current = *deck;
-	size_t size = 0, i = 0;
+	deck_node_t *current, *sorted = NULL;
+	int swapped;
 
-	while (current)
-		current = current->next, size++;
-
-	array = malloc(sizeof(deck_node_t *) * size);
-	if (!array)
+	if (!deck || !(*deck) || !(*deck)->next)
 		return;
 
-	for (i = 0, current = *deck; i < size; i++, current = current->next)
-		array[i] = current;
+	do {
+		swapped = 0;
+		current = *deck;
 
-	qsort(array, size, sizeof(deck_node_t *), compare_cards);
+		while (current->next != sorted)
+		{
+			if (current->card->kind > current->next->card->kind ||
+				(current->card->kind == current->next->card->kind &&
+				card_value(current->card->value) >
+				card_value(current->next->card->value)))
+			{
+				swap_nodes(&current, &current->next);
+				swapped = 1;
 
-	for (i = 0; i < size; i++)
-	{
-		array[i]->prev = i > 0 ? array[i - 1] : NULL;
-		array[i]->next = i < size - 1 ? array[i + 1] : NULL;
-	}
-
-	*deck = array[0];
-	free(array);
+				if (current->prev == NULL)
+					*deck = current;
+			}
+			if (current->next)
+				current = current->next;
+		}
+		sorted = current;
+	} while (swapped);
 }
