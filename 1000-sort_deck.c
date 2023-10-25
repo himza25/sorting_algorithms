539815d3206ad6ef2e5704dd7db829f0e49f8f1a
himza25 <himza.of.theking95@gmail.com>
2023-10-24 17:01:11 -0700

    Implement deck sort algorithm

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -1,43 +1,75 @@
 #include "deck.h"
+#include <stdlib.h>
 #include <string.h>
 
 /**
+ * compare_cards - Compares two cards.
+ * @a: First card.
+ * @b: Second card.
+ * Return: Difference in cards.
+ */
+int compare_cards(const void *a, const void *b)
+{
+	const deck_node_t *nodeA = *(const deck_node_t **)a;
+	const deck_node_t *nodeB = *(const deck_node_t **)b;
+	int valueA = strcmp(nodeA->card->value, nodeB->card->value);
+	int kindA = nodeA->card->kind;
+	int kindB = nodeB->card->kind;
+
+	if (kindA != kindB)
+		return (kindA - kindB);
+
+	if (valueA == 0)
+		return (0);
+
+	if (strcmp(nodeA->card->value, "Ace") == 0)
+		return (-1);
+	if (strcmp(nodeB->card->value, "Ace") == 0)
+		return (1);
+	if (strcmp(nodeA->card->value, "King") == 0)
+		return (1);
+	if (strcmp(nodeB->card->value, "King") == 0)
+		return (-1);
+	if (strcmp(nodeA->card->value, "Queen") == 0)
+		return (1);
+	if (strcmp(nodeB->card->value, "Queen") == 0)
+		return (-1);
+	if (strcmp(nodeA->card->value, "Jack") == 0)
+		return (1);
+	if (strcmp(nodeB->card->value, "Jack") == 0)
+		return (-1);
+
+	return (valueA);
+}
+
+/**
  * sort_deck - Sorts a deck of cards.
- * @deck: Double pointer to the deck.
+ * @deck: Deck to sort.
  */
 void sort_deck(deck_node_t **deck)
 {
-	deck_node_t *tmp, *min;
-	int i, j;
-	char *values[] = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10",
-	"Jack", "Queen", "King"};
+	deck_node_t *current = *deck;
+	deck_node_t **array;
+	size_t i = 0, size = 52;
 
-	if (deck == NULL || *deck == NULL)
+	array = malloc(sizeof(deck_node_t *) * size);
+	if (!array)
 		return;
 
-	for (tmp = *deck; tmp; tmp = tmp->next)
+	while (current)
 	{
-		min = tmp;
-		for (i = 0; i < 13; i++)
-		{
-			if (strcmp(tmp->card->value, values[i]) == 0)
-				break;
-		}
-		for (; tmp->next; tmp = tmp->next)
-		{
-			for (j = 0; j < 13; j++)
-			{
-				if (strcmp(tmp->next->card->value, values[j]) == 0)
-					break;
-			}
-			if (i > j)
-			{
-				min = tmp->next;
-				i = j;
-			}
-		}
-		if (min != tmp)
-			swap_nodes(deck, tmp, min);
+		array[i++] = current;
+		current = current->next;
 	}
-	sort_by_suits(deck);
+
+	qsort(array, size, sizeof(deck_node_t *), compare_cards);
+
+	for (i = 0; i < size; ++i)
+	{
+		array[i]->prev = i > 0 ? array[i - 1] : NULL;
+		array[i]->next = i < size - 1 ? array[i + 1] : NULL;
+	}
+
+	*deck = array[0];
+	free(array);
 }
